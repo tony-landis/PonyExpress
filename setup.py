05590aecbdfb990965254cd12ca676edcfd79227
Tony Landis <tony.landis@gmail.com>
2011-09-15 17:43:58 -0600

    new version, remove gearman dep

--- setup.py
+++ setup.py
@@ -2,17 +2,20 @@ from distutils.core import setup
 
 setup(
     name='PonyExpress',
-    version='0.12dev',
+    version='0.14stable',
 		author = 'Tony Landis',
 		author_email = 'tony.landis@gmail.com',
     packages=['ponyexpress',],
 		url = 'https://github.com/tony-landis/PonyExpress',
     license='Creative Commons Attribution-Noncommercial-Share Alike license',
+	description="""
+		High performance, transactional email message queuing, logging,\
+		and multi-lingual HTML and plain text templates management.
+		""",
     long_description=open('README.md').read(),
 		install_requires=[
 			'Flask',
 			'Flask-Actions',
-			'gearman',
 			'formencode',
 			'couchdbkit',
 		]
